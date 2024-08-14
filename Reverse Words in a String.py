3d310ee6668e7601284e361b8a35f3b235db39b9
thesooge <thesooge.8313@gmail.com>
2024-08-14 09:06:04 +0330

    reverse words in a string pr #151

--- Reverse Words in a String.py
+++ Reverse Words in a String.py
@@ -1,10 +1,17 @@
-s = "the sky is blue"
-
-s = bytearray()
-
-
-s[2] = 'b'
+s = "a good   example"
+new_str = ''
 
 
+s = s[::-1]
 
-print(s)        
+
+s = (s.split(' '))  
+for word in s:
+    if word == '':
+        continue
+    else:
+        new_str += word[::-1] + ' '   
+
+new_str = new_str.removesuffix(' ')
+print(new_str)
+    
